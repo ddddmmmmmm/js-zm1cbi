2c732054acaba82e2301aec104fb6688410a4d84
ddddmmmmmm <markova.dana@email.cz>
2022-01-22 10:17:32 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-console.log('ahoj');
+console.log('pepa');
 import './ukol.1.js';
