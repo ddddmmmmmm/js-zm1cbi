75dd0a371bf26f23f159d2c1591b1562d4e88587
ddddmmmmmm <markova.dana@email.cz>
2022-01-22 10:20:36 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-console.log('ahoj');
+console.log('nazdar');
+
