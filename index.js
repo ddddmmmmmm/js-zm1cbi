056885c062d8d5bbbf212bc8ce13288a7f824699
ddddmmmmmm <markova.dana@email.cz>
2022-01-22 11:05:58 +0100

    2 files modified

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 console.log('nazdar');
 
+
+//1) Založte proměnné pro jméno, vek, vahu v kg a a vysku v metrech - nastavte jim libovolnou hodnotu. Spočtěte BMI Do konzole vypište větu ve formátu:
+//"Jmenuji se Petra, je mi 34 let, vážím 65kg, měřím 1.67m a moje BMI je 23,2.".
+
+let jmeno ='petra';
+let vek = 34;
+let vaha = 65;
+let vyska = 1.67;
+let BMI = 
+console.log('Jmenuji se Petra, je mi 34 let, vážím 65 kg');
+
+
+
--- ukol1.js
+++ ukol1.js
@@ -1,6 +1,17 @@
 console.log('nazdar');
 
+
+//1) Založte proměnné pro jméno, vek, vahu v kg a a vysku v metrech - nastavte jim libovolnou hodnotu. Spočtěte BMI Do konzole vypište větu ve formátu: VÁHA / VÝŠKA NA DRUHOU (VÝŠKA KRÁT VÝŠKA)
+//"Jmenuji se Petra, je mi 34 let, vážím 65kg, měřím 1.67m a moje BMI je 23,2.".
+
 console.log('ahoj');
 
+
+let jmeno ='petra';
+let vek = 34;
+let vaha = 65;
+let vyska = 1.67;
+let BMI = vaha / (vyska*vyska);
+console.log('BMI');
 
 
