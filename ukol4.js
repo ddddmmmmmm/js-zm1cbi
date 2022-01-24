b4e9219b00b33b261371e4c2d891852e27de0db8
ddddmmmmmm <markova.dana@email.cz>
2022-01-24 10:59:49 +0100

    2 files modified

--- ukol4.js
+++ ukol4.js
@@ -1,9 +1,21 @@
 /*Napište funkci, která vypíše součet, rozdíl, násobek a podíl
 2 zadaných čísel, rozdíl zobrazte jako absolutni hodnotu*/
 
-let cislo = a;
-let csilo = b;
 
-function  soucet (a, b) {
-  let soucet a+b
+let input = Number(prompt("Zadej libovolné číslo:")); 
+
+function Vypis(a) {
+ for (i = 1; i <= a; i++) {
+  console.log(i)
+ };
 }
+
+Vypis(input);
+
+
+
+
+
+
+/*Holky, vzhledem k tomu, že Javascript není úplně jednoduchý, a zároveň věřím, že vám nemusí být vždycky příjemné se ptát (případně se v tomto tempu ani ptát nestíháte 😄) - posílám odkaz na nástroj sli.do, které bude otevřené až do pondělí 24.1. a můžete se ptát na cokoliv vás ještě napadne  —> https://app.sli.do/event/eKNHPgUTaJQjDPdzwuGLuS 😇.. Je to rychlejší a více user-friendly, než psaní po mailech (a opět - je to i anonymní) a všichni se vám tam budou snažit na všechny vaše dotazy odpovědět 🌷
+*/  
---
+++ ukol5.js
@@ -0,0 +1,4 @@
+/*vytvoř funkci do které jako vstup zadáme číslo a ona pomocí cyklu vypíše do konzole číselnou řadu do daného čísla. */
+
+let cislo = Number(prompt("Zadej libovolné číslo:")); 
+functiotin vypisCiselnouRadu 
